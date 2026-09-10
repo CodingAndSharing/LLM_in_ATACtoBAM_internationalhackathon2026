91811768c344eddc4bbf52bb6c6be335055a1c29
j042699 <josephimobio2@gmail.com>
2026-09-10 17:54:48 +0100

    init

---
+++ sra_explorer_fastq_download.sh
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR118/020/SRR11878620/SRR11878620_1.fastq.gz -o SRR11878620_GSM4579145_mESC_DMSO_ATAC_rep2_Mus_musculus_ATAC-seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR118/020/SRR11878620/SRR11878620_2.fastq.gz -o SRR11878620_GSM4579145_mESC_DMSO_ATAC_rep2_Mus_musculus_ATAC-seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR118/019/SRR11878619/SRR11878619_1.fastq.gz -o SRR11878619_GSM4579144_mESC_DMSO_ATAC_rep1_Mus_musculus_ATAC-seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR118/019/SRR11878619/SRR11878619_2.fastq.gz -o SRR11878619_GSM4579144_mESC_DMSO_ATAC_rep1_Mus_musculus_ATAC-seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR989/004/SRR9894854/SRR9894854_1.fastq.gz -o SRR9894854_GSM4005240_NPC48h_DMSO_ATAC_rep1_Mus_musculus_ATAC-seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR989/004/SRR9894854/SRR9894854_2.fastq.gz -o SRR9894854_GSM4005240_NPC48h_DMSO_ATAC_rep1_Mus_musculus_ATAC-seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR989/005/SRR9894855/SRR9894855_1.fastq.gz -o SRR9894855_GSM4005241_NPC48h_DMSO_ATAC_rep2_Mus_musculus_ATAC-seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR989/005/SRR9894855/SRR9894855_2.fastq.gz -o SRR9894855_GSM4005241_NPC48h_DMSO_ATAC_rep2_Mus_musculus_ATAC-seq_2.fastq.gz
